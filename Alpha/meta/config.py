f72e61995f202fdb464c23b1cfb3ab2716778eee
Omar CHIDA <chidaomar46@gmail.com>
2023-03-29 02:14:39 -0400

    Started work on PPO and integration with GYM

---
+++ Alpha/meta/config.py
@@ -0,0 +1,20 @@
+# stockstats technical indicator column names
+# check https://pypi.org/project/stockstats/ for different names
+INDICATORS = [
+    "macd",
+    "boll_ub",
+    "boll_lb",
+    "rsi_30",
+    "cci_30",
+    "dx_30",
+    "close_30_sma",
+    "close_60_sma",
+]
+
+
+PPO_PARAMS = {
+    "n_steps": 2048,
+    "ent_coef": 0.01,
+    "learning_rate": 0.00025,
+    "batch_size": 64,
+}
